18f19904f608089ff97d0af6b9c9ee2dd5286e46
fahadp <fahadp@gmail.com>
2015-03-08 18:28:40 -0700

    merged in regression and split

--- analyze.py
+++ analyze.py
@@ -19,7 +19,8 @@ def get_data(key,**kwargs):
 		raise ValueError('Data for key %s not found'%s)
 	#DATA KEYS: country	year	zeros	1	2	3	4	5	6	7	8	9	10	11	12
 	df = get_csv(key)
-	df = pd.concat([df,GT.loc[:,[key,'economic','internet','mobiles','urban','english']]],axis=1)
+	df['ground'] = GT[key]
+	df = pd.concat([df,GT.loc[:,['economic','internet','mobiles','urban','english']]],axis=1)
 	df = df.dropna() #Drop all rows with a nan in it
 
 	#filter rows with more than 5 zeros
@@ -34,6 +35,35 @@ def filter_years(df,year=2011):
 def filter_zeros(df):
 	zeros = df.loc[:,'zeros']
 	return df[zeros<=5]
+
+def runModel(features,data,reg=0,verbose=0):
+	if reg:
+		model = ols("ground ~ "+features, data).fit_regularized(alpha=0.2,maxiter=1000,Lt_wt=1)
+	else:
+		model = ols("ground ~ "+features, data).fit()
+	if verbose:
+		print model.summary()
+	else:
+		print model.rsquared#+model.params+model.pvalues
+	return model
+
+def getDataSplit(data,count):
+	print 'SIZE: %s'%data.shape[0]
+	idx = np.arange(data.shape[0])
+	np.random.shuffle(idx)
+	bin_size = math.ceil(data.shape[0]/float(count))
+	buckets = [idx[bin_size*i:bin_size*(i+1)] for i in xrange(count)]
+	for i in range(count):
+		frt = buckets[0:i]
+		end = buckets[i+1:]
+		front = []
+		for e in frt:
+			front.extend(e)
+		for e in end:
+			front.extend(e)
+		print front
+		yield data.iloc[front], data.iloc[buckets[i]]
+
 
 
 '''
