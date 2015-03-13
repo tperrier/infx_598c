be9c03898a57b57f516ceb610ca7c579977074a8
fahadp <fahadp@gmail.com>
2015-03-12 22:46:21 -0700

    get dataset stats function added

--- analyze.py
+++ analyze.py
@@ -34,6 +34,18 @@ def get_data(key,**kwargs):
 	df = filter_zeros(df,count=kwargs.get('count',5))
 	return df
 
+def get_dataset_stats(**kwargs):
+	for key in ['hiv','tb']:
+		df = get_csv(key)
+		total_size = df.shape[0]
+		df['ground'] = GT[key]
+		df = pd.concat([df,GT.loc[:,GT_LABELS]],axis=1)
+		df = df.dropna()
+		size_ground = df.shape[0]
+		df = filter_zeros(df,count=kwargs.get('count',5))
+		size_zero = df.shape[0]
+		print '%s,%s,%s,%s,%s'%(key,total_size,(total_size-size_ground),(size_ground-size_zero),size_zero)
+
 def filter_years(df,year=2011):
 	years = df.loc[:,'year']
 	return df[years>=year]
